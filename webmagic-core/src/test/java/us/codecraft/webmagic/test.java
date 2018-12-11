d112423bb949cc4d24cc463a93468dc5a9b03c42
lilingfei <lilingfei93@163.com>
2018-12-11 16:53:31 +0800

    test4

--- webmagic-core/src/test/java/us/codecraft/webmagic/test.java
+++ webmagic-core/src/test/java/us/codecraft/webmagic/test.java
@@ -1,11 +1,16 @@
 package us.codecraft.webmagic;
 
 public class test {
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
     public static void main(String[] args) {
         System.out.println("begin......hello......");
         System.out.println("hhhhhhhhhhhhhhhhhhhhhhhhhhh");
         for (int i = 0;i < 100;i++){
-            System.out.println("****************88当前的i是:"+i);
+            System.out.println("*************&&&&***88当前的i是:"+i);
         }
     }
 
