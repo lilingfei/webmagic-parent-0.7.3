eaeafdf5704bc5fa173b8585f50434b994495ab1
lilingfei <lilingfei93@163.com>
2018-12-11 15:59:20 +0800

    test1

--- webmagic-core/src/test/java/us/codecraft/webmagic/test.java
+++ webmagic-core/src/test/java/us/codecraft/webmagic/test.java
@@ -3,5 +3,6 @@ package us.codecraft.webmagic;
 public class test {
     public static void main(String[] args) {
         System.out.println("begin......");
+        System.out.println("hhhhhhhhhhhhhhhhhhhhhhhhhhh");
     }
 }
