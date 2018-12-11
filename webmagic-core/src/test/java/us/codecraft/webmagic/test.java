efde961ec05421fa388e5739b904e5632c8a4532
lilingfei <lilingfei93@163.com>
2018-12-11 16:48:44 +0800

    test3

--- webmagic-core/src/test/java/us/codecraft/webmagic/test.java
+++ webmagic-core/src/test/java/us/codecraft/webmagic/test.java
@@ -4,5 +4,13 @@ public class test {
     public static void main(String[] args) {
         System.out.println("begin......hello......");
         System.out.println("hhhhhhhhhhhhhhhhhhhhhhhhhhh");
+        for (int i = 0;i < 100;i++){
+            System.out.println("****************88当前的i是:"+i);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
     }
 }
