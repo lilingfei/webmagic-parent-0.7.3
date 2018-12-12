4271ef393c6faead40405d6c3ade62e0ff29abfc
lilingfei <lilingfei93@163.com>
2018-12-12 12:54:03 +0800

    test54

--- webmagic-core/src/test/java/us/codecraft/webmagic/test.java
+++ webmagic-core/src/test/java/us/codecraft/webmagic/test.java
@@ -6,16 +6,7 @@ public class test {
         return super.equals(obj);
     }
 
-    public static void main(String[] args) {
-        System.out.println("begin......hello......");
-        System.out.println("hhhhhhhhhhhhhhhhhhhhhhhhhhh");
-        for (int i = 0;i < 100;i++){
-            System.out.println("*************&&&&***88当前的i是:"+i);
-        }
-    }
-
-    @Override
-    public String toString() {
-        return super.toString();
+    public static  void test(){
+        System.out.println("ooooooooooooooooooooooooooooo");
     }
 }
