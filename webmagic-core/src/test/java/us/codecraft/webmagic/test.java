7990290fea54c7254ee4bdf4778df54db0a7c09c
lilingfei <lilingfei@users.noreply.github.com>
2018-12-13 12:31:35 +0800

    Update test.java

--- webmagic-core/src/test/java/us/codecraft/webmagic/test.java
+++ webmagic-core/src/test/java/us/codecraft/webmagic/test.java
@@ -1,17 +1,9 @@
 package us.codecraft.webmagic;
 
 public class test {
-    @Override
-    public boolean equals(Object obj) {
-        return super.equals(obj);
-    }
 
-    @Override
-    public int hashCode() {
-        return super.hashCode();
-    }
-
-    public static void gg(String[] args) {
+   public static void gg(String[] args) {
+        System.out.println("abb");
 
     }
 }
